98a864dba1ecac7e4be0e26484a8da339c204ae3
gerardoRmrz <grmzrso@outlook.com>
2026-08-03 12:31:33 -0500

    App structure has been changed to fit with specifications

---
+++ controllers/blogs.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+
+const { Blog } = require("../models");
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+  if (!req.blog) {
+    return res.status(404).end();
+  }
+  next();
+};
+
+router.get("/", async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+router.get("/id", blogFinder, async (req, res) => {
+  res.json(req.note);
+});
+
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  try {
+    const blog = await Blog.create({ ...req.body });
+    res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = await req.params;
+  try {
+    const blog = await Blog.destroy({
+      where: {
+        id: id,
+      },
+    });
+    res.status(204).json({});
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,99 +1,21 @@
-require("dotenv").config();
 const { defaults } = require("pg");
-const { Sequelize, Model, DataTypes, QueryTypes } = require("sequelize");
+
 const express = require("express");
 const app = express();
 
+const { PORT } = require("./util/config");
+const { connectToDatabase } = require("./util/db");
+const blogsRouter = require("./controllers/blogs");
+
 app.use(express.json());
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  port: 5432,
-  dialectOptions: {
-    ssl: false,
-  },
-});
+app.use("/api/blogs", blogsRouter);
 
-class Blog extends Model {}
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    author: {
-      type: DataTypes.STRING,
-    },
-    url: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    likes: {
-      type: DataTypes.INTEGER,
-      defaultValue: 0,
-    },
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: "blog",
-  },
-);
-
-Blog.sync();
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-app.get("/api/blogs", async (req, res) => {
-  const blogs = await Blog.findAll();
-  res.json(blogs);
-});
-
-app.post("/api/blogs", async (req, res) => {
-  console.log(req.body);
-  try {
-    const blog = await Blog.create({ ...req.body });
-    res.json(blog);
-  } catch (error) {
-    return res.status(400).json({ error });
-  }
-});
-
-app.delete("/api/blogs/:id", async (req, res) => {
-  const { id } = await req.params;
-  try {
-    const blog = await Blog.destroy({
-      where: {
-        id: id,
-      },
-    });
-    res.status(204).json({});
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-const main = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-    const blogs = await sequelize.query("SELECT * FROM blogs", {
-      type: QueryTypes.SELECT,
-    });
-    console.log(blogs);
-    // await sequelize.close(); // No poner cuando la app está en uso
-  } catch (error) {
-    console.error("Unable to connect to the database: ", error);
-  }
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 };
 
-main();
+start();
