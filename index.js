74edd3030f499bad09b3163363cce40a8e47623c
gerardoRmrz <grmzrso@outlook.com>
2026-08-24 11:12:05 -0500

    add logging

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.get("/", async (req, res) => {
 
 const start = async () => {
   await connectToDatabase();
-  app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
   });
 };
